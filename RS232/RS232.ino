456eb22767517b9458935d0a2787e60b65eba899
xs19283 <xs19283@gmail.com>
2018-06-10 20:04:16 +0800

    Arduino Tire pressure

--- RS232/RS232.ino
+++ RS232/RS232.ino
@@ -1,19 +1,32 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(2, 3); // RX, TX
+int TireArray[8];
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(38400);
   mySerial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int rd;
-  
-  if (mySerial.available()){
-    rd=mySerial.read();
-    Serial.print(rd);
-    Serial.print(" ");
-  }
-  Serial.println(" ");
+   int OutRd,Tire;
+    if (mySerial.available()){
+    OutRd = mySerial.read();
+    if(OutRd == 85){  //如果是讀到85 就開始放值
+      TireArray[0]=OutRd;
+      for(int i=1; i<8; i++){
+          TireArray[i] = mySerial.read();
+      }
+      delay(17.2);
+    }else{  //如果沒讀到就歸零
+      /*for(int i=0; i<TireNumber; i++){
+          TireArray[i]=0;
+      }*/
+    }
+    }
+    for(int i=0; i<8; i++){
+      Serial.print(TireArray[i]);
+      Serial.print("\t");
+    }
+    Serial.println(" ");
 }
