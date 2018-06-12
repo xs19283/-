707798c8081210f5c3d6808965643a21871fc8bb
xs19283 <xs19283@gmail.com>
2018-06-12 15:54:21 +0800

    master

--- HX711/HX711_master.ino
+++ HX711/HX711_master.ino
@@ -312,26 +312,7 @@ void BuleAndRed(){
 ////胎壓數值
 ////////////////////////////////////
 void TirePressure(){
-    int OutRd,Tire;
-    if (mySerial.available()){
-    OutRd = mySerial.read();
-    if(OutRd == 85){  //如果是讀到85 就開始放值
-      TireArray[0]=OutRd;
-      for(int i=1; i<8; i++){
-          TireArray[i] = mySerial.read();
-      }
-      delay(17.2);  //測到最好的數值
-    }else{  //如果沒讀到就歸零
-      /*for(int i=0; i<TireNumber; i++){
-          TireArray[i]=0;
-      }*/
-    }
-    }
-    for(int i=0; i<TireNUMBER; i++){
-      Serial.print(TireArray[i]);
-      Serial.print("\t");
-    }
-    Serial.println(" ");
+    
 }
 
 ////////////////////////////////////
