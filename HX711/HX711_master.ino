1743402ba95e0cbc4fdc06b7438e51973075dd8c
xs19283 <xs19283@gmail.com>
2018-07-16 22:54:08 +0800

    updata

--- HX711/HX711_master.ino
+++ HX711/HX711_master.ino
@@ -4,7 +4,6 @@
 #include <SoftwareSerial.h>
 #include <MsTimer2.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
 Servo myservo; // 建立Servo物件，控制伺服馬達
 //Timer T1;
 
@@ -24,8 +23,8 @@ int Seg = 1;
 int OnOff;
 int red, bule;
 
-const int INTERVAL = 27;
-const int TireTime = 27;  //每9ms做一次胎壓
+const int INTERVAL = 1;
+const int TireTime = 11;  //每9ms做一次胎壓
 const int AdxlTime = 0;
 const int Hx711Time = 0;
 
@@ -40,14 +39,14 @@ void setup() {
   ////////////////////////////////////
   ////設定Arduino鮑率及胎壓感測器鮑率
   ////////////////////////////////////
-  Serial.begin(115200);
-  mySerial.begin(9600);
+  Serial.begin(9600);
+  //mySerial.begin(9600);
 
   ////////////////////////////////////
   ////設定馬達腳位
   ////////////////////////////////////
   //myservo.attach(6, 500, 2400); // 修正脈衝寬度範圍
-  myservo.attach(6);
+  //myservo.attach(6);
 
   ////////////////////////////////////
   ////設定應變規初始設定
@@ -59,7 +58,7 @@ void setup() {
   ////////////////////////////////////
   ////設定胎壓定時中斷每0.9秒中斷一次
   ////////////////////////////////////
-  MsTimer2::set(INTERVAL, TirePressure);
+  MsTimer2::set(INTERVAL, MainTime);
   ////////////////////////////////////
   ////在程式開始時亮燈
   ////////////////////////////////////
@@ -78,36 +77,6 @@ void MainTime() {
 }
 
 
-
-
-////////////////////////////////////
-////馬達控制函式
-////////////////////////////////////
-void MotorCmd(int angle) {
-  switch (angle) {
-    case 1:
-      myservo.write(0);
-      break;
-    case 2:
-      myservo.write(20);
-      break;
-    case 3:
-      myservo.write(50);
-      break;
-    case 4:
-      myservo.write(75);
-      break;
-    case 5:
-      myservo.write(100);
-      break;
-    case 6:
-      myservo.write(125);
-      break;
-  }
-}
-
-
-
 ////////////////////////////////////
 ////胎壓數值
 ////////////////////////////////////
@@ -115,11 +84,21 @@ void TirePressure() {
   
   int out;
   do{
-    out = mySerial.read();
+    out = Serial.read();
+    if(out == -1){
+      break;
+    }else if(out == 85){
+      TireArray[0] = out;
+    }
   }while(out != 85);
-  TireArray[0] = out;
+  
   for(int i = 1; i < 8; i++){
-    TireArray[i] = mySerial.read();
+    int in = Serial.read();
+    if(in != -1){
+      TireArray[i] = in; 
+    }else{
+      break;
+    }
   }
   
   TirePrint();
@@ -149,7 +128,7 @@ void SwitchOnOff() {
     //CalculateByLoad();      ////用平均值判斷
     //CalculateByVariance();  ////標準差判斷
     //TirePressure();
-
+    
   } else {
   }
 }
@@ -160,30 +139,5 @@ void SwitchOnOff() {
 void loop() {
   SwitchOnOff();
 }
-/*
-  byte c[1];
-  int ctrl;
-  if(Serial.available())
-  {  //如果有接到資料 讀取字元並放入cmd字串 並轉為整數設定伺服馬達轉向
-    Serial.readBytes(c,1);
-    ctrl = atoi(c);
-    Serial.println(ctrl);
-  }
 
-  //SortArray陣列查看
-  Serial.print("SortArray \t");
-  for (int i=0; i<NUMBER; i++){
-    Serial.print("\t");
-    Serial.print(SortArray[i], 1);
-  }
-  Serial.println("\t");
-
-  //TireArray陣列查看
-  Serial.print("TireArray \t");
-  for (int i=0; i<TireNumber; i++){
-    Serial.print("\t");
-    Serial.print(TireArray[i], 1);
-  }
-  Serial.println("\t");
-*/
 
