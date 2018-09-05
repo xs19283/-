47446322ae8cfa7e3b8b5797a8999686272365bf
xs19283 <xs19283@gmail.com>
2018-09-05 20:52:41 +0800

    version7.1

--- 霍爾行程避震/________/________.ino
+++ 霍爾行程避震/________/________.ino
@@ -60,6 +60,9 @@ float q_bias, angle_err, PCt_0, PCt_1, E, K_0, K_1, t_0, t_1;
 float timeChange = 5; //滤波法采样时间间隔毫秒
 float dt = timeChange * 0.001; //注意：dt的取值为滤波器采样时间
 
+//////////////////////外部中斷控制變數//////////////////////
+int CtrlInti = 0;
+
 void setup() {
   Serial.begin(9600);
   //////////////////////設定開關跟按鈕//////////////////////
@@ -89,11 +92,13 @@ void setup() {
 
 //////////////////////外部中斷涵式//////////////////////
 void InteHall() {
+  CtrlInti = 1;
   if (SendX > 1000) {
     MotorCmd(2);
     NowMode = 5;
     delay(1000);
   }
+  CtrlInti = 0;
 }
 
 //////////////////////霍爾陣列初始化完放五個元素//////////////////////
@@ -261,14 +266,16 @@ void BlueAndRed() {
 
 //////////////////////藍芽傳值涵式//////////////////////
 void BluetoothSendData() {
-
-  Serial.write(85);
-  Serial.write(SendX);
-  Serial.write(SendZ);
-  Serial.write(SendHall);
-  Serial.write(int(angle));
-  Serial.write(NowMode);
-
+  int AccX=(ay/16384.0f)*100;
+  int AccZ=(az/16384.0f)*100;
+  
+    Serial.write(85);
+    Serial.write(AccX);
+    Serial.write(AccZ);
+    Serial.write(SendHall);
+    Serial.write(int(angle));
+    Serial.write(NowMode);
+  
   /*
     Serial.write(85);
     Serial.write(analogRead(XPin));
@@ -280,15 +287,15 @@ void BluetoothSendData() {
 
 //////////////////////模式選擇//////////////////////
 void NowModeSwitch() {
-  if (SendZ > 1000 && SendHall <= 20) {
+  if (SendZ > 500 && SendHall <= 4 && CtrlInti == 0) {
     MotorCmd(6);
     NowMode = 4;
-    delay(2000);
-  } else if (angle > 43) {
+    delay(1000);
+  } else if (angle > 43 && SendHall >= 5 && CtrlInti == 0) {
     MotorCmd(2);
     NowMode = 3;
     delay(1000);
-  } else if (SendZ > 400 && SendHall > 10) {
+  } else if (SendZ > 400 && SendHall > 10 && CtrlInti == 0) {
     if (SendHall > 10) {
       MotorCmd(3);
       NowMode = 2;
@@ -297,16 +304,16 @@ void NowModeSwitch() {
       MotorCmd(4);
       NowMode = 2;
       delay(500);
-    }else if(SendHall>50){
+    } else if (SendHall > 50) {
       MotorCmd(5);
       NowMode = 2;
       delay(500);
     }
-  } else if (SendX > 1000) {
+  } else if (SendX > 1000 && CtrlInti == 0) {
     MotorCmd(1);
     NowMode = 1;
     delay(2000);
-  } else if (SendX <= 1000) {
+  } else if (SendX <= 1000 && CtrlInti == 0) {
     MotorCmd(1);
     NowMode = 1;
     delay(500);
@@ -327,7 +334,7 @@ void SwitchOnOff() {
     VariancePulsByLoad(ZSortArray, &SendZ);
     VariancePulsByLoad(HallSortArray, &SendHall);
     Angletest();
-    NowModeSwitch();
+   // NowModeSwitch();
     BluetoothSendData();
   } else {
     BlueAndRed();
